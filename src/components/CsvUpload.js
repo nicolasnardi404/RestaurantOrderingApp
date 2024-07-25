0afca67eea517f9d5c284990b8d1f3d1fe66c45e
unknown <stagista02@A000004.netsurf.it>
2024-07-25 15:44:56 +0200

    refactor data builder to italian

--- src/components/CsvUpload.js
+++ src/components/CsvUpload.js
@@ -6,25 +6,29 @@
     const [files, setFiles] = useState([]);
     const [editableData, setEditableData] = useState([]);
     const [newItem, setNewItem] = useState({
-        dish: '',
-        date: '',
-        type: '',
+        nome: '',
+        data: '',
+        tipo: '',
     });
     const [fileDropped, setFileDropped] = useState(false);
 
     const sendDataToServer = async () => {
         try {
-          const response = await axios.post("http://localhost:80/project/draganddrop", editableData);
-          console.log(response.data);
-          // Reset state after successful send
-          setEditableData([]); // Clear the editable data
-          setNewItem({ dish: '', date: '', type: '' }); // Reset the new item state
-          setFiles([]); // Clear the files array
-          setFileDropped(false); // Re-enable the drag-and-drop area
+            const response = await axios.post("http://localhost:80/project/draganddrop", editableData, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            console.log(response.data);
+            // Reset state after successful send
+            setEditableData([]); // Clear the editable data
+            setNewItem({ nome: '', data: '', tipo: '' }); // Reset the new item state
+            setFiles([]); // Clear the files array
+            setFileDropped(false); // Re-enable the drag-and-drop area
         } catch (error) {
-          console.error(error.response.data); 
+            console.error(error.response.data); 
         }
-      };
+    };
 
     const handleDragOver = (event) => {
         event.preventDefault();
@@ -57,22 +61,22 @@
     };
 
     const organizeData = (data) => {
-        const dishes = [];
+        const nomes = [];
 
         data.forEach((row) => {
         const day = row["Giorno"] || "";
-        ["Primi", "Secondi", "Contorni"].forEach((type) => {
-            const dishesString = row[type] || "";
-            dishesString.split("\n")
-            .map((dish) => dish.trim())
+        ["Primi", "Secondi", "Contorni"].forEach((tipo) => {
+            const nomesString = row[tipo] || "";
+            nomesString.split("\n")
+            .map((nome) => nome.trim())
             .filter(Boolean)
-            .forEach((dish) => {
-                dishes.push({ dish: dish, date: day, type: type });
+            .forEach((nome) => {
+                nomes.push({ nome: nome, data: day, tipo: tipo });
             });
         });
         });
 
-        return dishes;
+        return nomes;
     };
 
     const handleDrop = async (event) => {
@@ -91,9 +95,9 @@
             const organizedData = organizeData(parsedData);
 
             const editableFormat = organizedData.map(item => ({
-            dish: item.dish,
-            date: item.date,
-            type: item.type,
+            nome: item.nome,
+            data: item.data,
+            tipo: item.tipo,
             }));
 
             setEditableData(editableFormat);
@@ -116,7 +120,7 @@
 
     const addItem = () => {
         setEditableData([...editableData, newItem]);
-        setNewItem({ dish: '', date: '', type: '' });
+        setNewItem({ nome: '', data: '', tipo: '' });
     };
     const handleInputChange = (index, fieldName) => (event) => {
         const updatedItems = [...editableData];
@@ -153,15 +157,15 @@
                 <tbody className='container-body'>
                   {editableData.map((item, index) => (
                     <tr className="container-row" key={index}>
-                      <td className='dish-item'>
+                      <td className='nome-item'>
                         <input
-                          type="text"
-                          value={item.dish}
-                          onChange={handleInputChange(index, 'dish')}
+                          tipo="text"
+                          value={item.nome}
+                          onChange={handleInputChange(index, 'nome')}
                         />
                       </td>
                       <td className='day-item'>
-                        <select value={item.type} onChange={(e) => handleInputChange(index, 'type')(e)}>
+                        <select value={item.tipo} onChange={(e) => handleInputChange(index, 'tipo')(e)}>
                           <option value="Lunedì">Lunedì</option>
                           <option value="Martedì">Martedì</option>
                           <option value="Mercoledì">Mercoledì</option>
@@ -169,8 +173,8 @@
                           <option value="Venerdì">Venerdì</option>
                         </select>
                       </td>
-                      <td className='type-item'>
-                        <select value={item.type} onChange={(e) => handleInputChange(index, 'tipo')(e)}>
+                      <td className='tipo-item'>
+                        <select value={item.tipo} onChange={(e) => handleInputChange(index, 'tipo')(e)}>
                           <option value="Primi">Primi</option>
                           <option value="Secondi">Secondi</option>
                           <option value="Contorni">Contorni</option>
@@ -191,7 +195,7 @@
                     addItem();
                 }}>
                     
-                    <button className='btn-classic' type="submit">Add Item</button>
+                    <button className='btn-classic' tipo="submit">Add Item</button>
                 </form>
                 <button className='btn-classic' onClick={sendDataToServer}>Submit All Data</button> 
               </div>
