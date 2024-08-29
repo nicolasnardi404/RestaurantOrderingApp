dff8bd244d1133e17440da14ef9bd10bc81fa6f6
unknown <stagista02@A000004.netsurf.it>
2024-08-29 15:08:43 +0200

    adjust to new json data

--- src/components/HistoricComponent.js
+++ src/components/HistoricComponent.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import { Button } from "primereact/button";
 
 
 export default function HistoricComponent() {
@@ -21,13 +22,14 @@ export default function HistoricComponent() {
     <div>
       <DataTable value={data.map(item => ({
         nome: item.username,
-        order_data: item.dataPrenotazione,
-        order: `${item.idPiatto} (${item.nome})`
+        order_data: item.reservation_date,
+        order: `${item.tipo_piatti}`
       }))} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} tableStyle={{ minWidth: '50rem' }}>
-        <Column field="nome" header="Nome" style={{ width: '25%' }}></Column>
-        <Column field="order-data" header="Giorno dell'Ordine" style={{ width: '25%' }} body={(rowData) => formatDate(rowData.order_data)}></Column>
-        <Column field="order" header="Ordine" style={{ width: '25%' }}></Column>
+        <Column field="nome" sortable header="Nome" style={{ width: '25%' }}></Column>
+        <Column field="order-data" sortable header="Giorno dell'Ordine" style={{ width: '25%' }} body={(rowData) => formatDate(rowData.order_data)}></Column>
+        <Column field="order" sortable header="Ordine" style={{ width: '25%' }}></Column>
       </DataTable>
+      <Button>Prenotazione per persona</Button>
     </div>
   );
 }
